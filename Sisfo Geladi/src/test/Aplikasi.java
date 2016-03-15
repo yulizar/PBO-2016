de1a597bbd2bed523820a8763755445ba5b68e8b
yulizar <Erwin@Yulizar>
2016-03-15 23:01:11 +0700

    Merge origin/master
    Conflicts:
    	Sisfo Geladi/src/Lokasi.java

---
+++ Sisfo Geladi/src/test/Aplikasi.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Erwin
+ */
+public class Aplikasi {
+    private ArrayList<Pembimbing> daftarPembimbing;
+    private ArrayList<Mahasiswa> daftarMahasiswa; // Pembimbing dan mahasiswa dapat dijadikan 1 array
+    private ArrayList<Lokasi> daftarLokasi; //Sudah ada array list kelompok di dalam lokasi, jadi ga perlu array list kelompok
+    
+    public void addPembimbing (Pembimbing p){
+        
+    }
+    
+    public void addMahasiswa (Mahasiswa m){
+        
+    }
+    
+    public void deleteMahasiswa (String nim){
+        
+    }
+    
+    public void menuSatu(){
+        
+    }
+    
+    public void menuDua(){
+        
+    }
+    
+    public void mainMenu(){
+        
+    }
+}
